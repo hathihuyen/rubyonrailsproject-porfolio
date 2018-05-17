eaf58022981ad6d82f16479db1aaaf98b72935e8
Huyen Ha <hathihuyen@gmail.com>
2018-05-17 11:51:46 -0700

    Updated status1 for enum, cannot change default for status column

---
+++ db/migrate/20180517184614_add_post_status1_to_blogs.rb
@@ -0,0 +1,5 @@
+class AddPostStatus1ToBlogs < ActiveRecord::Migration[5.2]
+  def change
+    add_column :blogs, :status1, :integer, default: 0
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_15_212629) do
+ActiveRecord::Schema.define(version: 2018_05_17_184614) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,7 +21,8 @@ ActiveRecord::Schema.define(version: 2018_05_15_212629) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "slug"
-    t.integer "status", default: 0
+    t.integer "status"
+    t.integer "status1", default: 0
     t.index ["slug"], name: "index_blogs_on_slug", unique: true
   end
 
