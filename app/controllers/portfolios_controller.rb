ee4089aa85bf0e49c41277ac942c3f49e9b3b1d6
Huyen Ha <hathihuyen@gmail.com>
2018-05-23 23:38:09 -0700

    Implemented partials with local data for portfolio form and nav bars

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -37,6 +37,7 @@ class PortfoliosController < ApplicationController
 
   def edit
     @portfolio_item = Portfolio.find(params[:id])
+    #3.times { @portfolio_item.technologies.build }
   end
   def update
     @portfolio_item = Portfolio.find(params[:id])
