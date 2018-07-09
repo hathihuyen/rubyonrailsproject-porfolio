f9135854f2e486143fbd3de9785e206ca62298d3
Huyen Ha <hathihuyen@gmail.com>
2018-07-08 20:05:01 -0700

    Implemented comment created on website

--- app/jobs/comment_broadcast_job.rb
+++ app/jobs/comment_broadcast_job.rb
@@ -3,6 +3,7 @@ class CommentBroadcastJob < ApplicationJob
 
   def perform(comment)
     ActionCable.server.broadcast "blogs_#{comment.blog.id}_channel", comment: render_comment(comment)
+  end
   
   private
    
