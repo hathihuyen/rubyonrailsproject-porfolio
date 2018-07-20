f17cbe00898a213b44c6119364f83621af07ea3a
Huyen Ha <hathihuyen@gmail.com>
2018-07-19 21:52:26 -0700

    Update seeds.rb file with admin and regular user

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,25 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.create!(
+  email: "test@test.com",
+  password: "abc123",
+  password_confirmation: "abc123",
+  name: "Admin User",
+  roles: "site_admin"
+)
+
+puts "1 Admin user created"
+
+User.create!(
+  email: "test1@test.com",
+  password: "abc123",
+  password_confirmation: "abc123",
+  name: "Regular User"
+)
+
+puts "1 regular user created"
+
 3.times do |topic|
   Topic.create!(
       title: "Topic #{topic}"
